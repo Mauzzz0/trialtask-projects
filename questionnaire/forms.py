793345f36f12c3c0f288e0a74e6cccc801b65eab
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-02-09 20:11:05 +0300

    del quizform

--- questionnaire/forms.py
+++ questionnaire/forms.py
@@ -1,9 +1,5 @@
 from .models import Quiz, Question
 from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
-class QuizForm(ModelForm):
-    class Meta:
-        model = Quiz
-        fields = ['title','date_start','date_end','description']
 
 
