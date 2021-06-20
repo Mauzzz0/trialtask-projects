fd8520f826c3cf7bce26be62af24fe01d3e6262e
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-06-20 19:30:46 +0300

    alpha

--- PryanikiTest/Services/OrderService.cs
+++ PryanikiTest/Services/OrderService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using MongoDB.Driver;
 using PryanikiTest.Base;
@@ -8,11 +9,13 @@ namespace PryanikiTest.Services
     public class OrderService
     {
         private readonly IMongoCollection<Order> _orders;
-        public OrderService(IProductDatabaseSettings settings)
+        private readonly ProductService _productService;
+        public OrderService(IProductDatabaseSettings settings, ProductService productService)
         {
             var client = new MongoClient(settings.ConnectionString);
             var db = client.GetDatabase(settings.DatabaseName);
 
+            _productService = productService;
             _orders = db.GetCollection<Order>(settings.OrdersCollectionName);
         }
         
@@ -24,20 +27,48 @@ namespace PryanikiTest.Services
 
         public Order Create(Order order)
         {
+            for (int i = 0; i < order.ProductId.Length; i++)
+            {
+                if (order.Quantity[i] > _productService.Get(order.ProductId[i]).Quantity)
+                {
+                    // Обработка некорректного ввода
+                }
+            }
+            
+            for (int i = 0; i < order.ProductId.Length; i++)
+            {
+                var product = _productService.Get(order.ProductId[i]);
+                product.Quantity -= order.Quantity[i];
+                _productService.Update(order.ProductId[i], product);
+            }
+            
+            
             _orders.InsertOne(order);
             return order;
         }
 
-        public void Update(string id, Order orderIn) => 
-            _orders.ReplaceOne(order => order.Id == id, orderIn);
+        public void Update(string id, Order orderIn) => throw new NotImplementedException();
+            // _orders.ReplaceOne(order => order.Id == id, orderIn);
 
-        public void Remove(Order orderIn) =>
-            _orders.DeleteOne(order => order.Id == orderIn.Id);
+        public void Remove(Order orderIn) => throw new NotImplementedException();
+            // _orders.DeleteOne(order => order.Id == orderIn.Id);
 
-        public void Remove(string id) => 
-            _orders.DeleteOne(order => order.Id == id);
+        public void Remove(string id)
+        {
+            var order = Get(id);
+            
+            for (int i = 0; i < order.ProductId.Length; i++)
+            {
+                var product = _productService.Get(order.ProductId[i]);
+                product.Quantity += order.Quantity[i];
+                _productService.Update(order.ProductId[i], product);
+            }
+            
+            _orders.DeleteOne(ord => ord.Id == id);
+            
+        }
 
-        public void Remove() =>
-            _orders.DeleteMany(order => true);
+        public void Remove() => throw new NotImplementedException();
+        // _orders.DeleteMany(order => true);
     }
 }
