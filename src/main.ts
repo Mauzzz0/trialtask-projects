48cc5b84c68b89e6c595c190e91003960b4658be
Ruslan <ruslan.semack1501@gmail.com>
2021-10-24 20:00:58 +0000

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ShowRoutesAtStart } from './main/helpers/ShowRoutes';
 import 'reflect-metadata';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   SetupSwagger(app);
 
   await app.listen(3000);
